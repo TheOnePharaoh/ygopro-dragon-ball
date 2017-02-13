dba6fd1bceb0884a4f00b3d8863bf048d5b79302
TheOnePharaoh <alaaz018@yahoo.com>
2017-02-13 13:41:16 -0600

    Freeza

---
+++ script/c96660003.lua
@@ -0,0 +1,43 @@
+--Freeza
+--Scripted by: TheOnePharaoh
+--This card cannot be Normal or Special Summoned except by the effect of "Freeza (Third Form)". Once per turn, if this card destroys an Opponent's Monster by battle: You can destroy all Defense Position monsters your opponent controls. Cannot be destroyed by card effects.
+function c96660003.initial_effect(c)
+	c:EnableReviveLimit()
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.FALSE)
+	c:RegisterEffect(e1)
+	--destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(70902743,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetCountLimit(1)
+	e2:SetCondition(aux.bdocon)
+	e2:SetTarget(c96660003.target)
+	e2:SetOperation(c96660003.operation)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+function c96660003.filter(c)
+	return not c:IsAttackPos()
+end
+function c96660003.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local g=Duel.GetMatchingGroup(c96660003.filter,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,g:GetCount(),0,0)
+end
+function c96660003.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c96660003.filter,tp,0,LOCATION_MZONE,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+end
