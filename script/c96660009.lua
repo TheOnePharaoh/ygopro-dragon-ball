90c18bacedd0363aa0b730b0ffc68a67e938375e
TheOnePharaoh <alaaz018@yahoo.com>
2017-02-14 17:17:24 -0600

    Fixed Jeice & Burter

--- script/c96660009.lua
+++ script/c96660009.lua
@@ -1,4 +1,6 @@
 --Jeice
+--Fixed by: TheOnePharaoh
+--If "Burter" is face-up on your side of the Field, this card gains 700 ATK and DEF.
 function c96660009.initial_effect(c)
 	--ATKUp
 	local e1=Effect.CreateEffect(c)
@@ -6,7 +8,8 @@ function c96660009.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c96660009.val)
+	e1:SetValue(700)
+	e1:SetCondition(c96660009.con)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENCE)
@@ -15,6 +18,6 @@ end
 function c96660009.filter(c)
 	return c:IsFaceup() and c:IsCode(96660010)
 end
-function c96660009.val(e,c)
-	return Duel.GetMatchingGroupCount(c96660009.filter,c:GetControler(),LOCATION_MZONE,0,nil)*700
+function c96660009.con(e,c)
+	return Duel.IsExistingMatchingCard(c96660009.filter,c:GetControler(),LOCATION_MZONE,0,nil)
 end
--- script/c96660010.lua
+++ script/c96660010.lua
@@ -1,4 +1,6 @@
 --Burter
+--Fixed by: TheOnePharaoh
+--If "Jeice" is face-up on your side of the Field, this card gains 700 ATK and DEF.
 function c96660010.initial_effect(c)
 	--ATKUp
 	local e1=Effect.CreateEffect(c)
@@ -6,7 +8,8 @@ function c96660010.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c96660010.val)
+	e1:SetValue(700)
+	e1:SetCondition(c96660010.con)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_UPDATE_DEFENCE)
@@ -15,6 +18,6 @@ end
 function c96660010.filter(c)
 	return c:IsFaceup() and c:IsCode(96660009)
 end
-function c96660010.val(e,c)
-	return Duel.GetMatchingGroupCount(c96660010.filter,c:GetControler(),LOCATION_MZONE,0,nil)*700
+function c96660010.con(e,c)
+	return Duel.IsExistingMatchingCard(c96660010.filter,c:GetControler(),LOCATION_MZONE,0,nil)
 end
