41b5d244f7bc35c1c00c8f389315eb32674eff5b
TheOnePharaoh <alaaz018@yahoo.com>
2017-02-16 11:56:38 -0600

    Dabura's Stone Spit fix

--- script/c96660021.lua
+++ script/c96660021.lua
@@ -12,10 +12,10 @@ function c96660021.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c96660021.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and c96660021.filter(chkc) end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,c96660021.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
 end
 function c96660021.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
